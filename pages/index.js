fd05e5e1724725e3259256b1f17e3ebfad5bf541
Chris Ware <c.p.ware@hotmail.com>
2022-10-07 16:54:01 -0700

    New MD Files/get MD Files from FS

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,57 @@
-import Head from "next/head";
-import Image from "next/image";
+import Link from 'next/link'
+import fs from "fs";
+import path from "path";
+import matter from "gray-matter";
 import Layout from "../components/Layout";
+import Post from '../components/Post';
+import {sortByDate} from "../utils/index.js";
 
-export default function Home() {
+export default function Home({ posts }) {
   return (
     <Layout>
-      <h1>Hello!</h1>
+      <h1 className='text-5xl border-b-4 p-5 font-bold'>Latest Posts</h1>
+
+      <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-5'>
+        {posts.map((post, index) => (
+          <Post key={index} post={post} />
+        ))}
+      </div>
+
+      <Link href='/blog'>
+        <a className='block text-center border border-gray-500 text-gray-800 rounded-md py-4 my-5 transition duration-500 ease select-none hover:text-white hover:bg-gray-900 focus:outline-none focus:shadow-outline w-full'>
+          All Posts
+        </a>
+      </Link>
     </Layout>
   );
+}
+
+export async function getStaticProps() {
+  // use the fs module to get the files from the directory:
+  const files = fs.readdirSync(path.join("posts"));
+
+  const posts = files.map((filename) => {
+    // create a slug:
+    const slug = filename.replace(".md", "");
+
+    //parese the front matter with the npm package gray matter
+    const markdownWithMeta = fs.readFileSync(
+      path.join("posts", filename),
+      "utf-8"
+    );
+
+    const { data: frontmatter } = matter(markdownWithMeta);
+
+    return {
+      slug,
+      frontmatter,
+    };
+  });
+
+  return {
+    props: {
+      posts: posts.sort(sortByDate).slice(0, 6),
+      // posts: posts.sort((a, b) => b.frontmatter.date - a.frontmatter.date),
+    },
+  };
 }
