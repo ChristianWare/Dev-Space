a57eb52b2d1636ffac8d4234f202cf76d1df210f
Chris Ware <c.p.ware@hotmail.com>
2022-11-30 08:26:32 -0700

    Filter Through Categories

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,32 @@
-import Link from 'next/link'
+import Link from "next/link";
 import fs from "fs";
 import path from "path";
 import matter from "gray-matter";
 import Layout from "../components/Layout";
-import Post from '../components/Post';
-import {sortByDate} from "../utils/index.js";
+import Post from "../components/Post";
+import { sortByDate } from "../utils/index.js";
 
 export default function Home({ posts }) {
   return (
     <Layout>
       <h1 className='text-5xl border-b-4 p-5 font-bold'>6 Recent Posts</h1>
 
+      <h2>Python</h2>
       <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-5'>
-        {posts.map((post, index) => (
-          <Post key={index} post={post} />
-        ))}
+        {posts
+          .filter((post) => post.frontmatter.category.includes("Python"))
+          .map((filteredCategory) => (
+            <Post post={filteredCategory} />
+          ))}
       </div>
-
+      <h2>JavaScript</h2>
+      <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-5'>
+        {posts
+          .filter((post) => post.frontmatter.category.includes("JavaScript"))
+          .slice(0,1).map((filteredCategory) => (
+            <Post post={filteredCategory} />
+          ))}
+      </div>
       <Link href='/blog'>
         <a className='block text-center border border-gray-500 text-gray-800 rounded-md py-4 my-5 transition duration-500 ease select-none hover:text-white hover:bg-gray-900 focus:outline-none focus:shadow-outline w-full'>
           All Posts
@@ -41,17 +51,18 @@ export async function getStaticProps() {
     );
 
     const { data: frontmatter } = matter(markdownWithMeta);
+    const category = frontmatter.category;
 
     return {
       slug,
       frontmatter,
+      category,
     };
   });
 
   return {
     props: {
       posts: posts.sort(sortByDate).slice(0, 6),
-      // posts: posts.sort((a, b) => b.frontmatter.date - a.frontmatter.date),
     },
   };
 }
